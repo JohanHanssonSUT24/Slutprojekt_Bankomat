407619924e877cbb3b95bfb847dbbab4955fb355
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-09-25 21:10:45 +0200

    New method - AccountBalance

--- Slutprojekt_Bankomat/Program.cs
+++ Slutprojekt_Bankomat/Program.cs
@@ -5,36 +5,28 @@
         static void Main(string[] args)
         {
 
-            int[] userNameArray = new int[5];
-            int[] userPassArray = new int[5];
-            userNameArray[0] = 850128;
-            userPassArray[0] = 1111;
-            userNameArray[1] = 890918;
-            userPassArray[1] = 2222;
-            userNameArray[2] = 100723;
-            userPassArray[2] = 3333;
-            userNameArray[3] = 180423;
-            userPassArray[3] = 4444;
-            userNameArray[4] = 230110;
-            userPassArray[4] = 5555;
+            int[] userNameArray = new int[] { 850128, 890918, 100723, 180423, 230110 };
+            int[] userPassArray = new int[] { 1111, 2222, 3333, 4444, 5555 };
 
             bool correctLoggin = false;
+            int userPass;
             int userLoggins = 0;
 
+            Console.WriteLine("Välkommen till DinBank!");
             Console.WriteLine("Skriv ditt personnummer: ");
             int userNumber = Int32.Parse(Console.ReadLine());
-
+            Console.WriteLine("Skriv ditt lösenord: ");
 
             while (!correctLoggin && userLoggins < 3)
             {
 
-                Console.WriteLine("Skriv ditt lösenord: ");
-                int userPass = Int32.Parse(Console.ReadLine());
+                userPass = Int32.Parse(Console.ReadLine());
                 if (userPass == userPassArray[Array.IndexOf(userNameArray, userNumber)])
                 {
                     bool menuBool = true;
                     while (menuBool)
                     {
+                        Console.Clear();
                         Console.WriteLine("Välkommen!");
                         Console.WriteLine("Var god välj ett av följande alternativ.");
                         Console.WriteLine("[1] Se dina konton och saldo");
@@ -42,26 +34,41 @@
                         Console.WriteLine("[3] Ta ut pengar");
                         Console.WriteLine("[4] Avsluta programmet");
 
-                        int userChoice = Int32.Parse(Console.ReadLine());
 
-                        switch (userChoice)
+
+                        int menuChoice;
+                        while (!Int32.TryParse(Console.ReadLine(), out menuChoice))
+                        {
+                            Console.WriteLine("Felaktig input!\nAnge alt 1 - 4. ");
+                        }
+
+                        switch (menuChoice)
                         {
                             case 1:
-                                
+                                Console.WriteLine("Dina konton och saldon");
+                                userNumber = AccountBalance();
+                                Console.ReadKey();
+                                //Metod för konton och saldo
                                 break;
 
                             case 2:
-
+                                Console.WriteLine("Intern överföring");
+                                Console.ReadKey();
+                                //Metod för överföring
                                 break;
 
                             case 3:
-
+                                Console.WriteLine("Ta ut pengar");
+                                Console.ReadKey();
+                                //Metod för uttag
                                 break;
 
                             case 4:
-                                Console.WriteLine("Tack för besöket. Vi ses nästa gång!");
+                                Console.Write("Tack för besöket. Vi ses nästa gång!");
                                 menuBool = false;
                                 break;
+
+
 
                         }
 
@@ -70,10 +77,10 @@
                 }
                 else
                 {
-                    Console.WriteLine("Felaktigt lösenord");
+                    Console.WriteLine("Felaktigt lösenord.");
                     userLoggins++;
                 }
-                
+
                 if (userLoggins == 3)
                 {
                     Console.WriteLine("Tyvärr, du har försökt logga in för många gånger!");
@@ -81,8 +88,19 @@
                 }
             }
 
+        }
+        public static void AccountBalance()
+        {
+            int[] userNameArray = new int[] { 850128, 890918, 100723, 180423, 230110 };
+            int[] userBalanceArray = new int[] { 22034, 101455, 11003, 8078, 3452 };
 
+            if (userNameArray[0])
+            {
+                Console.WriteLine(userBalanceArray[0]);
+            }
+            
 
+            
 
 
 
