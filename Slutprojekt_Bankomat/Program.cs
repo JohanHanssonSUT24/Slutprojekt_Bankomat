d8c804ac36ad07910a306c8a2b5b74c0e547e499
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-09-25 10:25:08 +0200

    Added new menu

--- Slutprojekt_Bankomat/Program.cs
+++ Slutprojekt_Bankomat/Program.cs
@@ -5,30 +5,61 @@
         static void Main(string[] args)
         {
 
-            string[] userNameArray = new string[5];
-            string[] userPassArray = new string[5];
-            userNameArray[0] = "Johan";
-            userPassArray[0] = "Ananas";
-            userNameArray[1] = "Jenny";
-            userPassArray[1] = "Banan";
-            userNameArray[2] = "Thea";
-            userPassArray[2] = "Äpple";
-            userNameArray[3] = "Tage";
-            userPassArray[3] = "Melon";
-            userNameArray[4] = "Otto";
-            userPassArray[4] = "Vindruva";
+            int[] userNameArray = new int[5];
+            int[] userPassArray = new int[5];
+            userNameArray[0] = 850128;
+            userPassArray[0] = 1111;
+            userNameArray[1] = 890918;
+            userPassArray[1] = 2222;
+            userNameArray[2] = 100723;
+            userPassArray[2] = 3333;
+            userNameArray[3] = 180423;
+            userPassArray[3] = 4444;
+            userNameArray[4] = 230110;
+            userPassArray[4] = 5555;
 
-            Console.WriteLine("Skriv ditt användarnamn: ");
-            string userNameInput = Console.ReadLine();
+            bool correctLoggin = false;
+            int userLoggins = 0;
 
-            Console.WriteLine(userNameArray[userNameInput]);
-            string userNameIndex = Console.ReadLine();
+            Console.WriteLine("Skriv ditt personnummer: ");
+            int userNumber = Int32.Parse(Console.ReadLine());
 
-            if (userNameIndex == userPassArray[userNameInput])
+            
+            while (!correctLoggin && userLoggins < 3)
             {
-                Console.WriteLine("Du skrev rätt!");
-
+                bool menuBool = true;
+                Console.WriteLine("Skriv ditt lösenord: ");
+                int userPass = Int32.Parse(Console.ReadLine());
+                if (userPass == userPassArray[Array.IndexOf(userNameArray, userNumber)])
+                {
+                    while (menuBool)
+                    {
+                        Console.WriteLine("Välkommen!");
+                        Console.WriteLine("Var god välj ett av följande alternativ.");
+                        Console.WriteLine("[1] Se dina konton och saldo");
+                        Console.WriteLine("[2] Överföring mellan konton");
+                        Console.WriteLine("[3] Ta ut pengar");
+                        Console.WriteLine("[4] Avsluta programmet");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Fel lösenord, försök igen.");
+                }
+                userLoggins++;
+                if (userLoggins == 3)
+                {
+                    Console.WriteLine("Tyvärr, du har försökt logga in för många gånger!");
+                    correctLoggin = true;
+                }
             }
+
+           
+            
+            
+
+          
 
 
 
