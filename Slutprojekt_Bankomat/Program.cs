cb0704d9b34e12923c929b8c68d7e882f6479930
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-10-07 21:15:35 +0200

    Fixed withdraw with password

--- Slutprojekt_Bankomat/Program.cs
+++ Slutprojekt_Bankomat/Program.cs
@@ -4,14 +4,11 @@ namespace Slutprojekt_Bankomat
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             //Arrays for users accountname, password
-
             int[] userNameArray = new int[] { 850128, 890918, 100723, 180423, 230110 };
             int[] userPassArray = new int[] { 1111, 2222, 3333, 4444, 5555 };
-
             //Jagged array for bankaccount names
             string[][] bankAccounts =
             {
@@ -30,184 +27,217 @@ namespace Slutprojekt_Bankomat
                 new double[] { 4054.55 },
                 new double[] { 1599.64 },
             };
-
-
             bool correctLoggin = false;
             int userPass;
             int userLoggins = 0;
-
+            int userNumber;
             while (!correctLoggin && userLoggins < 3)
             {
                 Console.WriteLine("Välkommen till DinBank!");
                 Console.WriteLine("Skriv ditt personnummer: ");
-                int userNumber = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Skriv ditt lösenord: ");
+                //int userNumber = Int32.Parse(Console.ReadLine());
 
-                userPass = Int32.Parse(Console.ReadLine());
-                if (userPass == userPassArray[Array.IndexOf(userNameArray, userNumber)])
+                if (!int.TryParse(Console.ReadLine(), out userNumber))
                 {
-                    bool menuBool = true;
-                    while (menuBool)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Välkommen!");
-                        Console.WriteLine("Var god välj ett av följande alternativ.");
-                        Console.WriteLine("[1] Se dina konton och saldo");
-                        Console.WriteLine("[2] Överföring mellan konton");
-                        Console.WriteLine("[3] Ta ut pengar");
-                        Console.WriteLine("[4] Logga ut");
-
-                        int menuChoice;
-                        while (!Int32.TryParse(Console.ReadLine(), out menuChoice))
-                        {
-                            Console.WriteLine("Felaktig input!\nAnge alt 1 - 4. ");
-                            Console.ReadKey();
-                        }
-
-                        switch (menuChoice)
-                        {
-                            case 1: //Konton och saldon
-                                Console.Clear();
-                                Console.WriteLine("Se konton och saldo:\n");
-                                Console.WriteLine("--------------------");
-                                AccountBalance(userNumber, userNameArray, bankAccounts, accountBalance);
-                                Console.WriteLine("\n\nKlicka på valfri tangent för att återvända till menyn.");
-                                Console.ReadKey();
-
-                                
-                                break;
-
-                            case 2: //Överföring
-                                Console.Clear();
-                                Console.WriteLine("Överföring");
-                                Console.WriteLine("-----------\n");
-                                TransferFunds(userNumber, userNameArray, bankAccounts, accountBalance);
-                                Console.WriteLine("\n\nKlicka på valfri tangent för att återvända till menyn.");
-                                Console.ReadKey();
-                                //Metod för överföring
-                                break;
-
-                            case 3: //uttag
-                                Console.Clear();
-                                Console.WriteLine("Uttag");
-                                Console.WriteLine("-------\n");
-                                WithdrawMoney(userNumber, userNameArray, bankAccounts, accountBalance);
-                                Console.WriteLine("\n\nKlicka på valfri tangent för att återvända till menyn.");
-                                Console.ReadKey();
-                                //Metod för uttag
-                                break;
-
-                            case 4: //Utloggning
-                                Console.Clear();
-                                Console.Write("Du loggas nu ut. Tack för besöket!");
-                                menuBool = false;
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-
-                            default:
-                                Console.WriteLine("Felaktig input. Välj alt 1-4!");
-                                Console.ReadKey();
-                                break;
-                        }
-                    }
+                    Console.WriteLine("Felaktig inmatning.");
+                }
+                else if (!Array.Exists(userNameArray, element => element == userNumber))
+                {
+                    Console.WriteLine("Personnumret finns inte i vårt register.");
                 }
                 else
                 {
-                    Console.WriteLine("Felaktigt lösenord.");
-                    userLoggins++;
-                }
+                    Console.WriteLine("Skriv ditt lösenord: ");
+                    userPass = Int32.Parse(Console.ReadLine());
+                    if (userPass == userPassArray[Array.IndexOf(userNameArray, userNumber)])
+                    {
+                        bool menuBool = true;
+                        while (menuBool)
+                        {   //Meny
+                            Console.Clear();
+                            Console.WriteLine("Välkommen!");
+                            Console.WriteLine("Var god välj ett av följande alternativ.");
+                            Console.WriteLine("[1] Se dina konton och saldo");
+                            Console.WriteLine("[2] Överföring mellan konton");
+                            Console.WriteLine("[3] Ta ut pengar");
+                            Console.WriteLine("[4] Logga ut");
+                            string userChoice = Console.ReadLine();
+                            switch (userChoice)
+                            {
+                                case "1": //Konton och saldon
+                                    Console.Clear();
+                                    Console.WriteLine("Se konton och saldo:\n");
+                                    Console.WriteLine("--------------------");
+                                    AccountBalance(userNumber, userNameArray, bankAccounts, accountBalance);
+                                    Console.WriteLine("\n\nKlicka på valfri tangent för att återvända till menyn.");
+                                    Console.ReadKey();
+                                    break;
 
-                if (userLoggins == 3)
-                {
-                    Console.WriteLine("Tyvärr, du har försökt logga in för många gånger!");
-                    correctLoggin = true;
-                }
+                                case "2": //Överföring
+                                    Console.Clear();
+                                    Console.WriteLine("Överföring");
+                                    Console.WriteLine("-----------\n");
+                                    TransferFunds(userNumber, userNameArray, bankAccounts, accountBalance);
+                                    Console.WriteLine("\n\nKlicka på valfri tangent för att återvända till menyn.");
+                                    Console.ReadKey();
+                                    //Metod för överföring
+                                    break;
 
+                                case "3": //uttag
+                                    Console.Clear();
+                                    Console.WriteLine("Uttag");
+                                    Console.WriteLine("-------\n");
+                                    WithdrawMoney(userNumber, userNameArray, userPassArray, bankAccounts, accountBalance);
+                                    Console.WriteLine("\n\nKlicka på valfri tangent för att återvända till menyn.");
+                                    Console.ReadKey();
+                                    //Metod för uttag
+                                    break;
+
+                                case "4": //Utloggning
+                                    Console.Clear();
+                                    Console.Write("Du loggas nu ut. Tack för besöket!");
+                                    menuBool = false;
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Felaktig input. Välj alternativ 1-4!");
+                                    Console.ReadKey();
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Felaktigt lösenord.");
+                        userLoggins++;
+                    }
+                    if (userLoggins == 3)
+                    {
+                        Console.WriteLine("Tyvärr, du har försökt logga in för många gånger!");
+                        correctLoggin = true;
+                    }
+                }
             }
         }
         public static void AccountBalance(int userName, int[] userNameArray, string[][] bankAccounts, double[][] accountBalance)
         {
-            int userIndex = Array.IndexOf(userNameArray, userName);
-
-            for (int i = 0; i < bankAccounts[userIndex].Length; i++)
+            int user = Array.IndexOf(userNameArray, userName);
+            for (int i = 0; i < bankAccounts[user].Length; i++)
             {
-                string bankAccount = bankAccounts[userIndex][i];
-                double balanceOfAccount = accountBalance[userIndex][i];
+                string bankAccount = bankAccounts[user][i];
+                double balanceOfAccount = accountBalance[user][i];
 
                 Console.WriteLine($"Konto: " + bankAccount);
                 Console.WriteLine("Saldo: " + balanceOfAccount);
                 Console.WriteLine("---------------------");
             }
         }
-
         public static void TransferFunds(int userName, int[] userNameArray, string[][] bankAccounts, double[][] accountBalance)
         {
-            int userIndex = Array.IndexOf(userNameArray, userName);
-
-            for (int i = 0; i < bankAccounts[userIndex].Length; i++)
+            int user = Array.IndexOf(userNameArray, userName);
+            for (int i = 0; i < bankAccounts[user].Length; i++)
             {
-                string bankAccount = bankAccounts[userIndex][i];
-                double balanceOfAccount = accountBalance[userIndex][i];
-
+                string bankAccount = bankAccounts[user][i];
+                double balanceOfAccount = accountBalance[user][i];
                 Console.WriteLine($"Konto: " + bankAccount);
                 Console.WriteLine($"Saldo: " + balanceOfAccount);
                 Console.WriteLine("---------------------");
             }
-
+            Console.WriteLine("Välj konto att flytta pengar ifrån: ");
+            int fromAccount = Int32.Parse(Console.ReadLine()) - 1;
+            Console.WriteLine("Välj konto att flytta pengar till: ");
+            int toAccount = Int32.Parse(Console.ReadLine()) - 1;
             Console.WriteLine("Skriv den summa du vill flytta över: ");
             double transferSum = double.Parse(Console.ReadLine());
-            Console.WriteLine("Välj konto att flytta pengar ifrån: ");
-            int fromAccount = Int32.Parse(Console.ReadLine())-1;
-            accountBalance[userIndex][fromAccount] = accountBalance[userIndex][fromAccount]-transferSum;
-            Console.WriteLine("Välj konto att flytta pengar till: ");
-            int toAccount = Int32.Parse(Console.ReadLine())-1;
-            accountBalance[userIndex][toAccount] = accountBalance[userIndex][toAccount] + transferSum;
 
-            Console.WriteLine("\nNytt saldo:\n");
-            for (int i = 0; i < accountBalance[userIndex].Length; i++)
+            if (transferSum <= 0 || transferSum > accountBalance[user][fromAccount])
             {
-                string bankAccount = bankAccounts[userIndex][i];
-                double balanceOfAccount = accountBalance[userIndex][i];
+                Console.WriteLine("Ogiltigt uttag. Du måste ange en summa högre än 0.");
+                Console.ReadKey();
+            }
+            else if (transferSum >= 0 || transferSum < accountBalance[user][fromAccount])
+            {
+                accountBalance[user][toAccount] = accountBalance[user][toAccount] + transferSum;
+                accountBalance[user][fromAccount] = accountBalance[user][fromAccount] - transferSum;
+            }
 
-
+            Console.Clear();
+            Console.WriteLine("Saldo:\n");
+            for (int i = 0; i < accountBalance[user].Length; i++)
+            {
+                string bankAccount = bankAccounts[user][i];
+                double balanceOfAccount = accountBalance[user][i];
                 Console.WriteLine($"Konto: " + bankAccount);
                 Console.WriteLine($"Saldo: " + balanceOfAccount);
                 Console.WriteLine("---------------------");
             }
+
+
         }
-        public static void WithdrawMoney(int userName, int[] userNameArray, string[][] bankAccounts, double[][] accountBalance)
+        public static void WithdrawMoney(int userName, int[] userNameArray, int[] userPassArray, string[][] bankAccounts, double[][] accountBalance)
         {
-            int userIndex = Array.IndexOf(userNameArray, userName);
+            bool passBool = true;
 
-            for (int i = 0; i < bankAccounts[userIndex].Length; i++)
+            int user = Array.IndexOf(userNameArray, userName);
+            for (int i = 0; i < bankAccounts[user].Length; i++)
             {
-                string bankAccount = bankAccounts[userIndex][i];
-                double balanceOfAccount = accountBalance[userIndex][i];
-
+                string bankAccount = bankAccounts[user][i];
+                double balanceOfAccount = accountBalance[user][i];
                 Console.WriteLine($"Konto: " + bankAccount);
                 Console.WriteLine($"Saldo: " + balanceOfAccount);
                 Console.WriteLine("---------------------");
             }
 
-            Console.WriteLine("Skriv den summa du vill ta ut: ");
-            double transferSum = double.Parse(Console.ReadLine());
             Console.WriteLine("Välj konto att ta pengar ifrån: ");
             int fromAccount = Int32.Parse(Console.ReadLine()) - 1;
-            accountBalance[userIndex][fromAccount] = accountBalance[userIndex][fromAccount] - transferSum;
-            
-
-            Console.WriteLine("\nNytt saldo:\n");
-            for (int i = 0; i < accountBalance[userIndex].Length; i++)
+            Console.WriteLine("Skriv den summa du vill ta ut: ");
+            double transferSum = double.Parse(Console.ReadLine());
+            int counterPass = 0;
+            while (passBool)
             {
-                string bankAccount = bankAccounts[userIndex][i];
-                double balanceOfAccount = accountBalance[userIndex][i];
+                Console.WriteLine("Ange lösenord för att bekräfta uttag: ");
+                int withdrawPass = Int32.Parse(Console.ReadLine());
 
+                if (withdrawPass != userPassArray[user] && counterPass < 3)
+                {
+                    Console.WriteLine("Fel lösenord.");
+                    counterPass++;
+                }
+                else
+                {
+                    Console.WriteLine("Uttag genomfört!");
+                    passBool = false;
+                }
+                if (counterPass == 3)
+                {
+                    Console.WriteLine("Du har skrivit fel lösenord 3 gånger. Transaktion avbruten.");
+                    passBool = false;
+                }
+            }
 
+            if (transferSum <= 0 || transferSum > accountBalance[user][fromAccount])
+            {
+                Console.WriteLine("Ogiltigt val. Du har inte tillräckligt med pengar.");
+            }
+            else if (transferSum >= 0 || transferSum < accountBalance[user][fromAccount])
+            {
+                accountBalance[user][fromAccount] = accountBalance[user][fromAccount] - transferSum;
+            }
+
+            Console.WriteLine("\nSaldo:\n");
+            for (int i = 0; i < accountBalance[user].Length; i++)
+            {
+                string bankAccount = bankAccounts[user][i];
+                double balanceOfAccount = accountBalance[user][i];
                 Console.WriteLine($"Konto: " + bankAccount);
                 Console.WriteLine($"Saldo: " + balanceOfAccount);
                 Console.WriteLine("---------------------");
+                passBool = false;
             }
+
+
         }
     }
 }
